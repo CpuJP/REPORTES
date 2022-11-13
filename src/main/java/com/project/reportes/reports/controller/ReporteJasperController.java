bb8ebf5963183a9d6912e16e95f447236fada075
CpuJP <cpujuanpis@gmail.com>
2022-11-12 22:37:14 -0500

    solución de CORS para conexión con angular

--- src/main/java/com/project/reportes/reports/controller/ReporteJasperController.java
+++ src/main/java/com/project/reportes/reports/controller/ReporteJasperController.java
@@ -10,15 +10,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @PreAuthorize("hasAnyRole('ADMIN', 'BIENS', 'BIBLIO', 'SALC', 'LABT', 'CAMP')")
 @RequestMapping("/report")
